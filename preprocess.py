5dd8c23de7382b886ecba51d51574f7456f44e87
keletso-mashabela <keletso.xavi2@gmail.com>
2025-07-01 01:44:32 +0200

    Create preprocess.py

---
+++ preprocess.py
@@ -0,0 +1,10 @@
+titanic-ml-project/
+├── data/                ← Your dataset CSV files go here (train.csv, test.csv)
+├── notebooks/           ← Your Kaggle `.ipynb` notebook file here
+├── src/                 ← Put the Python scripts here (new files you’ll create)
+│   ├── preprocess.py    ← First script (data cleaning & preparation)
+│   └── train_model.py   ← Second script (train model and make predictions)
+├── submission/          ← Predicted CSV file will be saved here
+├── README.md            ← Project description and instructions
+├── requirements.txt     ← List of dependencies
+└── .gitignore           ← Files/folders Git should ignore
